c6a453c5100dbe9efd36e4cc4e5ad2573dcaa263
Lokesh Velayudham - lowkeyy.eth <lokesh.beyondx@gmail.com>
2025-08-18 16:56:28 -0400

    Remove unused signIn import from SessionRecovery component to streamline code and improve clarity.

--- components/SessionRecovery.tsx
+++ components/SessionRecovery.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useSession, signIn } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 

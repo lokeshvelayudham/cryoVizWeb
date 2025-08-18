d23116e92651648bd1982f1f3239a500eb1d67b5
Lokesh Velayudham - lowkeyy.eth <lokesh.beyondx@gmail.com>
2025-08-18 14:07:22 -0400

    s

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,49 +1,24 @@
-// import "./globals.css";
-// import { ThemeProvider } from "@/components/theme-provider"
-// import ReactQueryProvider from "@/components/providers/ReactQueryProvider";
-// export const dynamic = "force-dynamic";
-// export const revalidate = 0;
-
-
-// export default function RootLayout({ children }: { children: React.ReactNode }) {
-//   return (
-//     <html lang="en" suppressHydrationWarning>
-//       <body>
-//         <ThemeProvider
-//           attribute="class"
-//           defaultTheme="system"
-//           enableSystem
-//           disableTransitionOnChange
-//         >
-//           <ReactQueryProvider>
-//             {children}
-//           </ReactQueryProvider>
-//         </ThemeProvider>
-//       </body>
-//     </html>
-//   );
-// }
-
 import "@/app/globals.css";
-import { Providers } from "@/app/providers";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/auth";
-
+import { ThemeProvider } from "@/components/theme-provider"
+import ReactQueryProvider from "@/components/providers/ReactQueryProvider";
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
-export const runtime = "nodejs";
 
-export default async function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const session = await getServerSession(authOptions);
 
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <Providers session={session}>{children}</Providers>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <ReactQueryProvider>
+            {children}
+          </ReactQueryProvider>
+        </ThemeProvider>
       </body>
     </html>
   );

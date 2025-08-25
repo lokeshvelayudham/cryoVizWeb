14ad42781625b7139285599dbebc589a85ce59d7
Lokesh Velayudham - lowkeyy.eth <lokesh.beyondx@gmail.com>
2025-08-25 15:05:46 -0400

    Make MongoDB connection conditional to prevent build-time connection attempts

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,15 +1,23 @@
 // lib/mongodb.ts
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI!;
-const options = {};
+// Only establish connection during runtime, not build time
+let clientPromise: Promise<MongoClient>;
 
-let client: MongoClient | undefined;
-const clientPromise: Promise<MongoClient> = (global._mongoClientPromise ?? (() => {
-  client = new MongoClient(uri, options);
-  global._mongoClientPromise = client.connect();
-  return global._mongoClientPromise;
-})()) as Promise<MongoClient>;
+if (typeof window === "undefined" && process.env.MONGODB_URI) {
+  const uri = process.env.MONGODB_URI;
+  const options = {};
+
+  let client: MongoClient | undefined;
+  clientPromise = (global._mongoClientPromise ?? (() => {
+    client = new MongoClient(uri, options);
+    global._mongoClientPromise = client.connect();
+    return global._mongoClientPromise;
+  })()) as Promise<MongoClient>;
+} else {
+  // Return a dummy promise during build time
+  clientPromise = Promise.resolve({} as MongoClient);
+}
 
 declare global {
   // allow global to have _mongoClientPromise

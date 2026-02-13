1c6e3131574eadd0b787d99ec7b575fa22a93a5b
Lokesh Velayudham - lowkeyy.eth <lokesh.beyondx@gmail.com>
2026-02-13 15:36:20 -0500

    Update Node.js engine requirement to 20.x.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  // output: 'standalone',
+  output: 'standalone',
   images: {
     formats: ['image/webp'],
     remotePatterns: [

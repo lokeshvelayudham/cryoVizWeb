5195272e6c3d89b977615b20253590a61d6ae03d
Lokesh Velayudham - lowkeyy.eth <lokesh.beyondx@gmail.com>
2026-02-26 17:25:11 -0500

    chore: remove MongoDB migration script.is

--- migrate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# URI with URL-encoded password (Primary Master Key - Write Access)
-URI="mongodb://cryovizweb-db:DrIyEOSQ3yGjrXK4tKeCeBpPtO6sPq6OdviGZXYuAOp9bnGnMWMxZx51gkVm2IGeS3vRzFwZrxD4ACDbpzbhgA%3D%3D@cryovizweb-db.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@cryovizweb-db@"
-
-echo "Restoring to Cosmos DB..."
-# Restore specific database CryoVizWeb
-mongorestore --uri="$URI" --nsInclude="CryoVizWeb.*" --dir=./mongo-dump --numParallelCollections=1 --numInsertionWorkersPerCollection=1 --batchSize=5
